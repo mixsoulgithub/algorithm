92bbbdbea56390f7590a2432f899eff7bc4c6a84
mix <2528770680@qq.com>
2026-02-27 21:29:33 +0800

    2148E

--- codeforce/2148E.cpp
+++ codeforce/2148E.cpp
@@ -2,33 +2,89 @@
 g++ --std=c++23 -o 2148E 2148E.cpp && cat 2148E.data|./2148E
 */
 #include <algorithm>
+#include <bit>
+#include <cmath>
+#include <cstdint>
 #include <iostream>
 #include <tuple>
 #include <unordered_map>
 #include <vector>
-#include <cmath>
-#include <bit>
 template <typename T> void dbg_out(T x) { std::cerr << x << ")" << std::endl; }
 void dbg_out() { std::cerr << ")" << std::endl; }
 template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
   std::cerr << H << ", ";
   dbg_out(T...);
 }
+#ifdef DEBUG
 #define debug(...)                                                             \
   std::cerr << "(" << #__VA_ARGS__ << ") = (", dbg_out(__VA_ARGS__)
-
+#else 
+#define debug(...) 
+#endif
 typedef unsigned long long ll_t;
 typedef std::vector<long long> vll_t;
 
 int main() {
   using namespace std;
+cin.tie(0)->sync_with_stdio(0);
   ll_t batch;
   cin >> batch;
-  for (;batch>0;batch--) {
-    unordered_map<int, int> map_1;
-    unordered_map<int, int> map_all;
-    
-
+    debug("1");
+  for (; batch > 0; batch--) {
+    int l = 0, r = 0;
+    int len, k;
+    cin >> len;
+    cin >> k;
+    debug(len,k);
+    uint64_t ans=1;
+    vector<int> map_all(len+1,0);
+    int nums[len];
+    for (int i = 0; i < len; i++) {
+      int e;
+      cin >> e;
+      nums[i] = e;
+      map_all[e]++;
+    }
+    bool has_not_k_times_element = false;
+    debug("1");
+    for (const auto &pair : map_all) {
+      if ((pair % k) != 0) {
+        has_not_k_times_element = true;
+        break;
+      }
+    }
+    if (has_not_k_times_element) {
+      cout << 0 << endl;
+      continue;
+    }
+    vector<int> map_lr(len+1,0);
+    // for(const auto& pair: map_all){
+    //   debug(pair.first,pair.second);
+    // }
+    map_lr[nums[0]] = 1;
+    // debug(0, map_lr[nums[0]]);
+    // debug(1, map_lr[nums[1]]);
+    while (true) {
+      if(r==len-1) break;
+      //  debug(r, map_lr[nums[r]],map_lr[nums[r+1]],map_all[nums[r+1]], ans);
+      if ((map_lr[nums[r+1]]+1) * k <= map_all[nums[r+1]]) {
+        r++;
+        ans+=r-l+1;
+        map_lr[nums[r]]++;
+      //  debug(r, map_lr[nums[r]],ans);
+      } else {
+        while (l<r) {
+          map_lr[nums[l]]--;
+          if(nums[l]==nums[r]){
+            l++;
+            break;
+          }else{
+            l++;
+          }
+        }
+      }
+    }
+    cout << ans<<endl;
   }
   return 0;
 }
--- codeforce/letsgo.sh
+++ codeforce/letsgo.sh
@@ -5,4 +5,4 @@ echo "usage: letsgo PROBLEM_ID"
 cp template.cpp $1.cpp
 touch $1.data 
 sed -i "s/problem_id/$1/g" $1.cpp
-code $1.cpp $1.data
+code $1.cpp $1.data $1.md
