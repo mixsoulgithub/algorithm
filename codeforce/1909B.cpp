8090898ac6408209d7b6c1f8e40883bf9e36b9c5
mix <2528770680@qq.com>
2026-03-01 23:58:42 +0800

    1909Bwrong

--- codeforce/1909B.cpp
+++ codeforce/1909B.cpp
@@ -1,19 +1,16 @@
 /*
-g++ -DDEBUG --std=c++23 -o problem_id.out problem_id.cpp && cat problem_id.data|./problem_id.out
+g++ --std=c++23 -o 1909B.out 1909B.cpp && cat 1909B.data|./1909B.out
 */
 #include <bits/stdc++.h>
+#include <cstdio>
 template <typename T> void dbg_out(T x) { std::cerr << x << ")" << std::endl; }
 void dbg_out() { std::cerr << ")" << std::endl; }
 template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
   std::cerr << H << ", ";
   dbg_out(T...);
 }
-#ifdef DEBUG
 #define debug(...)                                                             \
   std::cerr << "(" << #__VA_ARGS__ << ") = (", dbg_out(__VA_ARGS__)
-#else
-#define debug(...)
-#endif
 
 typedef unsigned long long ll_t;
 typedef std::vector<long long> vll_t;
@@ -22,7 +19,21 @@ using namespace std;
 int main() {
   ll_t batch;
   cin >> batch;
-  for (;batch>0;batch--) {
+  for (; batch > 0; batch--) {
+    int len;
+    cin >> len;
+    // debug(len);
+    ll_t first;
+    cin >> first;
+    // printf("0x%llx\n",first);
+    int zeros = 65;
+    for (; len > 1; len--) {
+      ll_t now;
+      cin >> now;
+    // printf("0x%llx\n",now);
+      zeros = min(zeros, std::countr_zero(now ^ first));
+    }
+    cout << (1 << (zeros + 1)) << endl;
   }
   return 0;
 }
