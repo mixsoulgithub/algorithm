49d6a6985e29a122ee6fcb9a232a3ad163f1181b
mix <2528770680@qq.com>
2026-05-24 19:24:29 +0800

    graph

--- classic/graph.hpp
+++ classic/graph.hpp
@@ -13,79 +13,140 @@
 template <typename Node, typename EdgeInfo = bool>
   requires std::equality_comparable<Node> && std::equality_comparable<EdgeInfo>
 struct MatrixGraph {
-  bool directed;
-  uint64_t nodes_num;
-  EdgeInfo invalid_edge;
-  // todo: if is not directed, use other structure to store node and edge, save
-  // space
-  std::vector<Node> nodes;
-  static constexpr bool is_simple_edge = std::is_same_v<EdgeInfo, bool>;
+  using node_type = Node;
+  using edge_type = EdgeInfo;
   using INT = uint64_t;
 
-
+  bool directed;
+  EdgeInfo invalid_edge;
+  std::vector<Node> nodes;
   std::vector<std::vector<EdgeInfo>> edges;
 
+  static constexpr bool is_simple_edge_v = std::is_same_v<EdgeInfo, bool>;
+
   MatrixGraph(EdgeInfo invalid_edge, bool d = false)
-      : invalid_edge(invalid_edge), directed(d), nodes_num(0)
-        {
+      : invalid_edge(invalid_edge), directed(d) {
   }
 
   INT insert_node(Node node) {
     nodes.push_back(node);
-    return nodes.size();
-  }
-  
-  bool delete_node(std::vector<INT> nodes_index) {
-    // trade off of lazy delete(this need)
-    INT size=nodes.size();
-    std::vector<bool> node_bitmap(size);
-    for(auto i:nodes_index){
-      node_bitmap[i]=true;
+    INT new_size = nodes.size();
+    for (auto &row : edges) {
+      row.resize(new_size, invalid_edge);
     }
-    INT offset=0;
-    for(INT i=0;i<size;){
-      if(node_bitmap[i]==true){
-        offset++;
-      }
-      if(!offset){
-        nodes[i]=nodes[i+offset];
-        edges[i]=edges[i+offset];
+    edges.resize(new_size, std::vector<EdgeInfo>(new_size, invalid_edge));
+    return new_size - 1;
+  }
+
+  bool delete_node(std::vector<INT> nodes_index) {
+    if (nodes_index.empty()) return false;
+    
+    INT size = nodes.size();
+    std::vector<bool> node_bitmap(size, false);
+    for (auto i : nodes_index) {
+      if (i < size) node_bitmap[i] = true;
+    }
+
+    std::vector<Node> new_nodes;
+    std::vector<std::vector<EdgeInfo>> new_edges;
+    std::vector<INT> old_to_new(size, 0);
+
+    for (INT i = 0; i < size; i++) {
+      if (!node_bitmap[i]) {
+        old_to_new[i] = new_nodes.size();
+        new_nodes.push_back(nodes[i]);
       }
     }
 
+    for (INT i = 0; i < size; i++) {
+      if (!node_bitmap[i]) {
+        std::vector<EdgeInfo> new_row;
+        for (INT j = 0; j < size; j++) {
+          if (!node_bitmap[j]) {
+            new_row.push_back(edges[i][j]);
+          }
+        }
+        new_edges.push_back(std::move(new_row));
+      }
+    }
+
+    nodes = std::move(new_nodes);
+    edges = std::move(new_edges);
+    return true;
   }
 
   void insert_edge(INT from, INT to, EdgeInfo info) {
-    if constexpr (is_simple_edge) {
-      edges[from][to] = 0b1;
+    if (from >= nodes.size() || to >= nodes.size()) return;
+    if constexpr (is_simple_edge_v) {
+      edges[from][to] = EdgeInfo{true};
     } else {
       edges[from][to] = info;
+    }
+    if (!directed) {
+      if constexpr (is_simple_edge_v) {
+        edges[to][from] = EdgeInfo{true};
+      } else {
+        edges[to][from] = info;
+      }
     }
   }
 
   Node &get_node(INT id) { return nodes[id]; }
-  INT get_node_id(Node node) {
-    INT i = 0;
-    for (auto &&cur : nodes) {
-      if (cur == node) {
+  const Node &get_node(INT id) const { return nodes[id]; }
+
+  INT get_node_id(Node node) const {
+    for (INT i = 0; i < nodes.size(); i++) {
+      if (nodes[i] == node) {
         return i;
       }
-      i++;
     }
+    return static_cast<INT>(-1);
   }
-  bool is_directed() { return directed; }
+
+  bool is_directed() const { return directed; }
+  INT nodes_count() const { return nodes.size(); }
 
   template <typename F>
-    requires std::invocable<F, Node>
+    requires std::invocable<F, Node&>
   void visit_nodes(F &&func) {
-    for (uint64_t i = 0; i < nodes_num; i++) {
-      if (!func(nodes[i])) {
-        return;
-      }
+    for (auto &node : nodes) {
+      func(node);
     }
   }
 
-  EdgeInfo get_edge(INT from, INT to) { return edges[from][to]; }
+  template <typename F>
+    requires std::invocable<F, const Node&>
+  void visit_nodes(F &&func) const {
+    for (const auto &node : nodes) {
+      func(node);
+    }
+  }
+
+  EdgeInfo get_edge(INT from, INT to) const {
+    if (from >= edges.size() || to >= edges[from].size()) {
+      return invalid_edge;
+    }
+    return edges[from][to];
+  }
+
+  bool has_edge(INT from, INT to) const {
+    if constexpr (is_simple_edge_v) {
+      return get_edge(from, to);
+    } else {
+      return get_edge(from, to) != invalid_edge;
+    }
+  }
+
+  std::vector<INT> get_neighbors(INT node_id) const {
+    std::vector<INT> neighbors;
+    if (node_id >= edges.size()) return neighbors;
+    for (INT i = 0; i < edges[node_id].size(); i++) {
+      if (has_edge(node_id, i)) {
+        neighbors.push_back(i);
+      }
+    }
+    return neighbors;
+  }
 };
 
 // 为你的图特化
--- classic/graph_algorithm.hpp
+++ classic/graph_algorithm.hpp
@@ -1,33 +1,138 @@
 #pragma once
 #include "graph.hpp"
+#include <queue>
+#include <vector>
+#include <type_traits>
+#include <functional>
+#include <algorithm>
 
 template<typename G>
 concept Graph = requires(G g) {
-    // Type requirements
     typename G::INT;
     typename G::Node;
     typename G::EdgeInfo;
+    typename G::node_type;
+    typename G::edge_type;
     
-    // Property access
-    { g.nodes_num } -> std::convertible_to<uint64_t>;
-    { g.directed } -> std::convertible_to<bool>;
+    { g.nodes_count() } -> std::convertible_to<typename G::INT>;
+    { g.is_directed() } -> std::convertible_to<bool>;
     
-    // Node operations
     { g.insert_node(std::declval<typename G::Node>()) } -> std::convertible_to<typename G::INT>;
-    { g.delete_node(std::declval<typename G::Node>()) } -> std::convertible_to<typename G::INT>;
     { g.get_node(std::declval<typename G::INT>()) } -> std::same_as<typename G::Node&>;
-    
-    // Edge operations
+    { g.get_neighbors(std::declval<typename G::INT>()) } -> std::same_as<std::vector<typename G::INT>>;
+    { g.has_edge(std::declval<typename G::INT>(), std::declval<typename G::INT>()) } -> std::same_as<bool>;
     { g.insert_edge(std::declval<typename G::INT>(), std::declval<typename G::INT>(), std::declval<typename G::EdgeInfo>()) } -> std::same_as<void>;
-    { g.delete_edge(std::declval<typename G::INT>(), std::declval<typename G::INT>(), std::declval<typename G::EdgeInfo>()) } -> std::same_as<void>;
     { g.get_edge(std::declval<typename G::INT>(), std::declval<typename G::INT>()) } -> std::same_as<typename G::EdgeInfo>;
-    
-    // Traversal
-    g.visit_nodes([](typename G::Node&){});
 };
 
+template <Graph G, typename F>
+  requires std::invocable<F, typename G::Node&, typename G::INT> ||
+           std::invocable<F, typename G::Node&>
+void bfs(G &graph, typename G::INT start_id, F &&visit_func) {
+    using Node = typename G::Node;
+    using INT = typename G::INT;
+    
+    INT node_count = graph.nodes_count();
+    if (start_id >= node_count) return;
+    
+    std::vector<bool> visited(node_count, false);
+    std::queue<INT> q;
+    
+    q.push(start_id);
+    visited[start_id] = true;
+    
+    while (!q.empty()) {
+        INT current_id = q.front();
+        q.pop();
+        
+        Node &current_node = graph.get_node(current_id);
+        
+        if constexpr (std::is_invocable_v<F, Node&, INT>) {
+            std::invoke(visit_func, current_node, current_id);
+        } else {
+            std::invoke(visit_func, current_node);
+        }
+        
+        std::vector<INT> neighbors = graph.get_neighbors(current_id);
+        for (INT neighbor_id : neighbors) {
+            if (!visited[neighbor_id]) {
+                visited[neighbor_id] = true;
+                q.push(neighbor_id);
+            }
+        }
+    }
+}
+
+template <Graph G, typename F>
+  requires std::invocable<F, typename G::Node&, typename G::INT> ||
+           std::invocable<F, typename G::Node&>
+void dfs(G &graph, typename G::INT start_id, F &&visit_func) {
+    using Node = typename G::Node;
+    using INT = typename G::INT;
+    
+    INT node_count = graph.nodes_count();
+    if (start_id >= node_count) return;
+    
+    std::vector<bool> visited(node_count, false);
+    
+    auto dfs_helper = [&](auto &&self, INT current_id) -> void {
+        visited[current_id] = true;
+        
+        Node &current_node = graph.get_node(current_id);
+        
+        if constexpr (std::is_invocable_v<F, Node&, INT>) {
+            std::invoke(visit_func, current_node, current_id);
+        } else {
+            std::invoke(visit_func, current_node);
+        }
+        
+        std::vector<INT> neighbors = graph.get_neighbors(current_id);
+        for (INT neighbor_id : neighbors) {
+            if (!visited[neighbor_id]) {
+                self(self, neighbor_id);
+            }
+        }
+    };
+    
+    dfs_helper(dfs_helper, start_id);
+}
 
 template <Graph G>
-void bfs(G graph){
-    auto first=graph.get_node();
+std::vector<typename G::INT> shortest_path_bfs(G &graph, typename G::INT start_id, typename G::INT target_id) {
+    using INT = typename G::INT;
+    
+    INT node_count = graph.nodes_count();
+    if (start_id >= node_count || target_id >= node_count) return {};
+    
+    std::vector<bool> visited(node_count, false);
+    std::vector<INT> parent(node_count, static_cast<INT>(-1));
+    std::queue<INT> q;
+    
+    q.push(start_id);
+    visited[start_id] = true;
+    
+    while (!q.empty()) {
+        INT current_id = q.front();
+        q.pop();
+        
+        if (current_id == target_id) {
+            std::vector<INT> path;
+            for (INT cur = target_id; cur != static_cast<INT>(-1); cur = parent[cur]) {
+                path.push_back(cur);
+            }
+            std::reverse(path.begin(), path.end());
+            return path;
+        }
+        
+        std::vector<INT> neighbors = graph.get_neighbors(current_id);
+        for (INT neighbor_id : neighbors) {
+            if (!visited[neighbor_id]) {
+                visited[neighbor_id] = true;
+                parent[neighbor_id] = current_id;
+                q.push(neighbor_id);
+            }
+        }
+    }
+    
+    return {};
 }
